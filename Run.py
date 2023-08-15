e74cf0cb8e6a021e8e3648e7e95cf6336dcc7902
Sora-3nc <142339684+Sora-3nc@users.noreply.github.com>
2023-08-16 00:18:12 +0700

    Add files via upload

---
+++ Run.py
@@ -0,0 +1,21 @@
+import os, platform
+
+import requests
+
+from os import system as cmd 
+
+os.system('git pull')
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    from C64 import main
+
+    main()
+
+elif bit == '32bit':
+
+    from C32 import main
+
+    main()
+
